33850d70e2c462da5461dd6b68f8ce8f34278dff
victor delgado <virtor.dsales@gmail.com>
2024-02-24 19:50:44 -0300

    final version

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,13 +2,25 @@ export const dataFake = [
   {
     "id":"1",
     "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "description": "Marvel anuncia um novo filme do homem de ferro, confira!",
     "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
   },
   {
     "id":"2",
     "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
+    "description": "Nova série anunciada na plataforma, confira!",
     "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Novo filme do pantera negra lançado em breve",
+    "description": "Revelada data do lançamento do novo filme do MCU, Pantera Negra!",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"4",
+    "title": "Novo filme dos X-men está em pós produção, afirma diretor",
+    "description": "Filmes dos X-men está próximo de ser lançado nos cinemas!",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
   }
 ]
